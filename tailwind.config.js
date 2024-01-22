339079e5b2eb722c7642cbd1b38565dcf18842a8
pedrosrc <pedroleon.pc@outlook.com>
2024-01-22 15:37:55 -0300

    chore: configuracao do tailwind [86a1zx3p7]

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,21 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        'grayish-blue': 'hsl(218, 22%, 67%)',
+        'cyan': 'hsl(179, 62%, 43%)',
+        'cyan-light': '#4ABEBD',
+        'bright-yellow': 'hsl(71, 73%, 54%)',
+        'light-gray': 'hsl(204, 43%, 93%)'
+      },
+      width: {
+        120: '40rem'
+      },
+      fontFamily: {
+        'sans': ['"Karla"','system-ui', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
